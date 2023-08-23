25fc39fd1f3b5db57c3c20462bab44c7ea2f4475
Mirko Zoranovic <mirko.zoranovic@vegait.rs>
2023-08-23 16:51:06 +0200

    Start project

---
+++ src/components/Timer.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemaining }) {
+  useEffect(function () {
+    setInterval(function () {
+      dispatch({ type: "tick" });
+    }, 1000);
+  }, []);
+
+  return <div className="timer">{secondsRemaining}</div>;
+}
+
+export default Timer;
